61c71adc73394539406d4b7affcffdf8deb80266
Dhananjay Panage <dhananjaypanage11@gmail.com>
2021-10-13 18:38:54 +0530

    added support for cp,mv and cat commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,58 @@
+
+#include <algorithm>
+#include <cstdio>
 #include <cstdlib>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <stdlib.h>
 #include <string.h>
 #include <string>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <vector>
+// Added history feature
 
-class InBuiltCommands {
+// TODO: Implement "mv" command
+// TODO: Implement "cp" command
+// TODO: Add redirection operations
+// TODO: Add pipe operator
+
+class history {
+private:
+  int index;
+
+public:
+  std::vector<std::pair<int, std::string>> list;
+  void add(std::string);
+  void show(int);
+  void show();
+  history();
+};
+history::history() { index = 1; }
+void history::add(std::string string) {
+  list.push_back({index, string});
+  index++;
+}
+void history::show(int num) {
+  if (num <= 0) {
+    std::cerr << "Enter number above 1\n";
+    return;
+  }
+  for (size_t i = 0; i < static_cast<size_t>(num); i++) {
+    std::cout << list[i].first << " " << list[i].second << "\n";
+  }
+}
+void history::show() {
+  for (auto &it : list) {
+    std::cout << it.first << " " << it.second << "\n";
+  }
+}
+
+history hist;
+class builtInCommands {
   // TODO Implement these methods and try to learn about these
 public:
   void cd();
@@ -22,12 +67,13 @@ std::string dshell_read_line() {
   return line;
 }
 
-std::vector<std::string> dshell_split_line(std::string str) {
+std::vector<std::string> split_line_with_delimiter(std::string str,
+                                                   char split) {
 
   std::vector<std::string> tokens;
   std::string::size_type whitespace, current = 0;
 
-  while ((whitespace = str.find(" ", current)) != std::string::npos) {
+  while ((whitespace = str.find(split, current)) != std::string::npos) {
     tokens.push_back(str.substr(current, whitespace - current));
     current = whitespace + 1;
   }
@@ -36,11 +82,89 @@ std::vector<std::string> dshell_split_line(std::string str) {
   return tokens;
 }
 
+bool check_if_command_exists(char *command) {
+  std::string path = getenv("PATH");
+  std::vector<std::string> paths = split_line_with_delimiter(path, ':');
+  struct stat stats;
+  for (auto &loc : paths) {
+    loc = loc.append("/");
+    if (stat(loc.append(command).c_str(), &stats) == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+void change_directory(char *current_directory, char **argv, size_t size) {
+  if (size == 1) {
+    chdir(getenv("HOME"));
+  } else if (argv[1][0] == '/') {
+    chdir(argv[1]);
+  } else if (argv[1][0] == '.') {
+    chdir(argv[1]);
+  } else if (argv[1][0] == '~') {
+    chdir(getenv("HOME"));
+  } else if (argv[1] != nullptr) {
+    strcat(current_directory, argv[1]);
+    chdir(current_directory);
+  }
+}
+void get_history(char **argv, size_t size) {
+  if (size == 2) {
+    hist.show(std::stoi(argv[1]));
+  } else {
+    hist.show();
+  }
+}
+void copy_file(char **argv, size_t size, bool is_to_be_deleted) {
+  // Working on cp command
+  if (size != 3) {
+    std::cerr << "Error: Can't copy!\nUsage: cp source destination";
+    return;
+  }
+  // Can copy from absolute path only
+
+  std::fstream read(argv[1], std::ios::in);
+  std::fstream write(argv[2], std::ios::out);
+  std::stringstream sstream;
+
+  if (read.fail()) {
+    std::cerr << "No such file or directory " << argv[1];
+    return;
+  }
+  if (write.fail()) {
+    std::cerr << "No such file or directory " << argv[2];
+    return;
+  }
+  sstream << read.rdbuf();
+  write.write(sstream.str().c_str(), static_cast<long>(sstream.str().length()));
+  sstream.clear();
+  if (is_to_be_deleted) {
+    std::remove(argv[1]);
+  }
+  read.close();
+  write.close();
+}
+void cat(char **argv, size_t size) {
+
+  if (size == 1) {
+    std::cerr << "Error: Operand required\nUsage:cp filename\n";
+    return;
+  }
+  std::fstream file(argv[1], std::ios::in);
+
+  if (file.is_open()) {
+    std::cout << file.rdbuf();
+    file.close();
+  } else {
+    std::cerr << "Error: No such file exists\n";
+    return;
+  }
+}
 int dshell_execute(std::vector<std::string> args) {
 
   pid_t pid, wpid;
-  int status;
   pid = fork();
+  int status;
 
   // Why the hell char* argv[] was causing error and not this one
   char **argv = (char **)malloc(sizeof(char *) * args.size());
@@ -50,53 +174,67 @@ int dshell_execute(std::vector<std::string> args) {
     argv[i] = const_cast<char *>(args[i].c_str());
   }
   if (pid == 0) {
-    char buffer[1024];
-    getcwd(buffer, 1024);
-    char *current_directory = buffer;
-    if ((strcmp(argv[0], "cd") == 0)) {
-      if (args.size() == 1) {
-        chdir(getenv("HOME"));
-      } else if (argv[1][0] == '/') {
-        chdir(argv[1]);
-      } else if (argv[1][0] == '.') {
-        chdir(argv[1]);
-      } else if (argv[1][0] == '~' || strcmp(argv[1], "") == 0) {
-        chdir(getenv("HOME"));
-      } else if (argv[1] != nullptr) {
-        strcat(current_directory, "/");
-        strcat(current_directory, argv[1]);
-        chdir(current_directory);
-      }
-    } else if (strcmp(argv[0], "ls") == 0) {
-      execvp("ls", argv);
+    char *current_directory = get_current_dir_name();
+    /* Required inorder to change directory suppose you are at /path (i.e
+     current directory )and if you didn't append "/" at the end then the path
+     becomes /pathfile instead of path/file */
+    strcat(current_directory, "/");
+    if ((strcmp(argv[0], "history")) == 0) {
+      get_history(argv, args.size());
+    } else if ((strcmp(argv[0], "cd") == 0)) {
+      change_directory(current_directory, argv, args.size());
+    } else if (!check_if_command_exists(argv[0])) {
+      std::cerr << "dshell: command not found: " << argv[0] << "\n";
+      return -1;
+    } else if (strcmp(argv[0], "cp") == 0) {
+      copy_file(argv, args.size(), false);
+    } else if (strcmp(argv[0], "cat") == 0) {
+      cat(argv, args.size());
+    } else if (strcmp(argv[0], "mv") == 0) {
+      copy_file(argv, args.size(), true);
     } else {
       execvp(argv[0], argv);
+      std::cerr << "Some error occured";
     }
   } else {
-    do {
-      wpid = waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    wpid = waitpid(pid, &status, WUNTRACED);
   }
+  free(argv);
   return 1;
 }
 
+std::string get_current_directory() {
+  char buff[1024];
+  getcwd(buff, 1024);
+  std::string current_directory(buff);
+  std::string to_remove = "/home/dhananjay";
+  size_t loc = current_directory.find(to_remove);
+  if (loc < current_directory.length()) {
+    current_directory.replace(loc, to_remove.size(), "~");
+  }
+  current_directory.append(" $ ");
+  return current_directory;
+}
+void add_to_history(std::string line) {
+  if (line.find("history") == std::string::npos) {
+    hist.add(line);
+  }
+}
 void dshell_loop() {
 
   std::string line;
   std::vector<std::string> args;
   int status = 0;
-  char buff[1024];
   do {
-    getcwd(buff, 1024);
-    std::cout << buff << " $ ";
+    std::cout << get_current_directory();
     line = dshell_read_line();
-    args = dshell_split_line(line);
+    add_to_history(line);
+    args = split_line_with_delimiter(line, ' ');
     status = dshell_execute(args);
   } while (status != 0);
 }
 
-int main(int argc, char **argv) {
-
+int main(void) {
   dshell_loop();
   return 0;
 }
